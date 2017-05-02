b7eef93757d8d5f73196a23786d531a8d3926c05
WizaXxX <wizax0@gmail.com>
2017-05-02 16:43:25 +0300

    Add progress ring and test tabcontrol source

--- 1CServicesControl/1CServicesControl/MainWindow.xaml.cs
+++ 1CServicesControl/1CServicesControl/MainWindow.xaml.cs
@@ -21,9 +21,60 @@ namespace _1CServicesControl
     /// </summary>
     public partial class MainWindow : MetroWindow
     {
+
+        public List<Server> listSrv;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            listSrv = new List<Server>();
+
+        }
+
+        public class Customer
+        {
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public int NumberOfContracts { get; set; }
+        }
+
+        private void MetroWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            listSrv.Add(new Server("Server1", "1123/12..123.", false, "", ""));
+            listSrv.Add(new Server("Server1", "1123/12..123.", false, "", ""));
+            listSrv.Add(new Server("Server1", "1123/12..123.", false, "", ""));
+            listSrv.Add(new Server("Server1", "1123/12..123.", false, "", ""));
+            listSrv.Add(new Server("Server1", "1123/12..123.", false, "", ""));
+
+            MainTabControl.ItemsSource = listSrv;
+            MainTabControl.SelectedIndex = 0;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+            //MainTabControl.ItemsSource = listSrv;
         }
     }
+
+
+    public class Server
+    {
+        public String name { get; set; }
+        public String address { get; set; }
+        public String login { get; set; }
+        public String pass { get; set; }
+        public Boolean isDomainAuth { get; set; }
+
+        public Server(String newName, String newAddress, Boolean newIsDomainAuth, String newLogin, String newPass)
+        {
+            name = newName;
+            address = newAddress;
+            login = newLogin;
+            pass = newPass;
+            isDomainAuth = newIsDomainAuth;
+        }
+    }
+
 }
